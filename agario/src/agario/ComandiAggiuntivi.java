8479729b099b371d27845f8ed76bad808f3ad254
EliSiro <tommyzonca12@gmail.com>
2022-05-16 09:54:57 +0200

    creazione nuova classe per eventi w e spazio

---
+++ agario/src/agario/ComandiAggiuntivi.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package agario;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+/**
+ *
+ * @author Sucone
+ */
+public class ComandiAggiuntivi implements KeyListener{
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_SPACE) //se premo spazio
+        {
+            
+        }
+        if (e.getKeyCode() == KeyEvent.VK_W)// se premo w
+        {
+            
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
